19bd7abd8b4a68e86182d9e6721799d39d2902c0
Sergey <isb9797@gmail.com>
2022-09-14 12:17:29 +0300

    PropsTypes

--- src/components/BurgerIngredients/BurgerIngridients.jsx
+++ src/components/BurgerIngredients/BurgerIngridients.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Box } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Typography } from "@ya.praktikum/react-developer-burger-ui-components";
